5741a0f0df7d92bba0521628cbf838c616a9c94d
MadhusudanDesai1 <desai02madhusudan@gmail.com>
2025-04-16 13:46:41 +0530

    initialized lexer

--- src/lexer/lexer.py
+++ src/lexer/lexer.py
@@ -1,13 +1,56 @@
 from lexer.token import Token
 
 class Lexer:
+    datatypes = ["integer", "real", "boolean", "string", "gameObject"]
+
+    keyword = [
+            "initGame",
+            "endGame",
+            "if",
+            "else", 
+            "while",
+            "break",
+            "continue",
+            "setBackgroudColor",
+            "initWindow",
+            "setWindowTitle",
+            "show",
+            "setWindowSize"
+    ]
+    
+    arithmeticOperators = [
+        '+', '-', '/', '*', '%' 
+    ]
+    
+    openParenthesis = [
+        '(', '{'
+    ]
+
+    closeParenthesis = [
+        ')', '}'
+    ]
+
+    specialCharacters = [
+        ','
+    ]
+
+    skippables = [
+        '\n', ' ', '\t'
+    ]          
+            
+
     def __init__(self) -> None:
         self.text : str = ""
         self.ptr : int = 0
+
 
     def tokenize(self, text : str) -> None:
         self.text = text
         self.ptr = 0
 
     def getNextToken(self) -> Token:
-        pass
+        curToken : Token
+
+        if value in self.dataTypes:
+            pass
+        
